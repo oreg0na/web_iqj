40bd7451668dadea419d5ebea76bc5345ab92850
Slava Pogosov <pogoslav1@gmail.com>
2024-05-02 09:14:20 +0300

    start scheme

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="IQJ - Admin panel">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
