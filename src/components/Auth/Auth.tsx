f97a4a2eae63258e0f6c5a91f26e0af88b53c3fa
Slava Pogosov <pogoslav1@gmail.com>
2024-05-03 19:13:55 +0300

    add valid email, password, button

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,8 +1,62 @@
+import {useState, ChangeEvent, FocusEvent, useEffect} from 'react'
 import React from 'react'
-import { motion } from 'framer-motion'
+import {motion} from 'framer-motion'
 import './Auth.scss'
 
 const Login: React.FC = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passwordDirty, setPasswordDirty] = useState(false)
+
+    const [emailError, setEmailError] = useState('Email не может быть пустым')
+    const [passwordError, setPasswordError] = useState('Пароль не может быть пустым')
+
+    const [formValid, setFormValid] = useState(false)
+
+    useEffect(() => {
+        if(emailError || passwordError){
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    }, [emailError, passwordError])
+
+    const emailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Некорректный email')
+        } else {
+            setEmailError("")
+        }
+    }
+
+    const passwordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        // @ts-ignore
+        setPassword(e.target.value)
+        if(e.target.value.length < 3 || e.target.value.length > 20){
+            setPasswordError('Пароль должен быть длиннее 3 и меньше 20')
+            if(!e.target.value){
+                setPasswordError('Пароль не может быть пустым')
+            }
+        } else {
+            setPasswordError('')
+        }
+    }
+
+    const blurHandler = (e: FocusEvent<HTMLInputElement, Element>) => {
+        switch (e.target.name){
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPasswordDirty(true)
+                break
+        }
+    }
     return (
         <div className='container'>
             <motion.div
@@ -22,10 +76,12 @@ const Login: React.FC = () => {
             <div className='form-container sign-in'>
                 <form>
                     <h1>Войти</h1>
-                    <input type='email' placeholder='Email' />
-                    <input type='password' placeholder='Пароль' />
+                    {(emailDirty && emailError) && <div style={{color:'red'}}>{emailError}</div>}
+                    <input onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} name='email' type='email' placeholder='Email' />
+                    {(passwordDirty && passwordError) && <div style={{color:'red'}}>{passwordError}</div>}
+                    <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} name='password' type='password' placeholder='Пароль' />
                     <a href='#'>Забыли пароль?</a>
-                    <button>Sign In</button>
+                    <button disabled={!formValid} type='submit'>Sign In</button>
                 </form>
             </div>
         </div>
