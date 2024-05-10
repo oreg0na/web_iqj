2e0303065971fc8e48db0b558bdbe2517eca88ba
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-05-10 20:32:31 +0300

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react'
 import Login from './pages/Auth/Auth'
-import News from './pages/News/WriteNews'
 import NotFound from './pages/NotFound/NotFound'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from 'react-router-dom'
 import './App.scss'
 import { useAppDispatch, useAppSelector } from './store/store'
 import { setIsLogin } from './store/slices/dataSlice'
 import axios from 'axios'
+import UserList from './pages/UserList/UserList'
+import EditUser from './pages/EditUser/EditUser'
+import NewsList from './pages/NewsList/NewsList'
+import EditNews from './pages/EditNews/EditNews'
 
 const App: React.FC = () => {
     const data = useAppSelector(state => state.data)
     const dispatch = useAppDispatch()
-
-    useEffect(() => {
-
-    }, [])
 
     useEffect(() => {
         if (['false', null].includes(localStorage.getItem('isLogin'))) {
@@ -44,9 +43,18 @@ const App: React.FC = () => {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path='/'>
-                <Route index element={<Navigate to='/login'/>}/>
-                <Route path='/login' element={isNotlogin(<Login/>, '/news')} />
-                <Route path='/news' element={isLogin(<News />, '/login')} />
+                <Route index element={<Navigate to='/login' />} />
+                <Route path='/login' element={isNotlogin(<Login />, '/user')} />
+                <Route path='/user'>
+                    <Route index element={<Navigate to='/user/list' />} />
+                    <Route path='/user/list' element={isLogin(<UserList />, '/login')} />
+                    <Route path='/user/edit' element={isLogin(<EditUser />, '/login')} />
+                </Route>
+                <Route path='/news'>
+                    <Route index element={<Navigate to='/news/list' />} />
+                    <Route path='/news/list' element={isLogin(<NewsList />, '/login')} />
+                    <Route path='/news/edit' element={isLogin(<EditNews />, '/login')} />
+                </Route>
                 <Route path='*' element={<NotFound />} />
             </Route>
         )
