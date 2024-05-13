0997ff7ebeffc5ad43cfe33a08c4efe0756eaf0c
Slava Pogosov <pogoslav1@gmail.com>
2024-05-14 00:21:22 +0300

    update title

---
+++ src/api/news.ts
@@ -0,0 +1,27 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import axios, { AxiosError } from 'axios'
+
+export interface News {
+    id: string
+    header: string
+    link: string
+    text: string
+    image_link: string
+    tags: string
+    publication_time: string
+}
+
+export const getNews = createAsyncThunk('news/get', async (data: any, thunkAPI) => {
+    try {
+        const response = await axios.get<Array<News>>('https://mireaiqj.ru:8443/news', {
+            withCredentials: true
+        })
+        return thunkAPI.fulfillWithValue(response.data)
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const axiosError = error as AxiosError
+            return thunkAPI.rejectWithValue(axiosError.message)
+        }
+        return thunkAPI.rejectWithValue('Unknown axios error')
+    }
+})
--- src/components/News/NewsList/NewsList.tsx
+++ src/components/News/NewsList/NewsList.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
+import {getNews} from "../../../api/news";
 import './NewsList.scss'
+import { addNotification } from '../../../store/slices/notificationSlice'
+import { useNavigate } from 'react-router-dom'
+
 const NewsList = () => {
+    const navigate = useNavigate()
+
+    const [roleFilter, setRoleFilter] = useState(false)
+
     return (
-        <div className='user-edit-container'>
+        <div className='news-list-container'>
             <div className='title-block'>
                 <span className='title'>News List</span>
+                <div className='filters-block'>
+                    <div onClick={() => setRoleFilter(true)} className='filter-block'>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
