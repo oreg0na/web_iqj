89a1eea91574e471c15aec006dcc1d0b478dde7d
Slava Pogosov <pogoslav1@gmail.com>
2024-05-18 03:04:34 +0300

    update edit news

--- src/components/News/EditNews/EditNews.tsx
+++ src/components/News/EditNews/EditNews.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
 import './EditNews.scss'
-import { useParams } from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
+
+import GoBackIcon from './assets/GoBackIcon.svg'
 
 const EditNews: React.FC = () => {
     const params = useParams<{id: string}>()
+    const navigate = useNavigate()
 
     return (
         <div className='news-edit-container'>
             <div className='title-block'>
                 <span className='title'>News Edit {params.id}</span>
+            </div>
+            <div className='edit-block'>
+                <img onClick={() => navigate('/panel/news/list')} className='back-icon' src={GoBackIcon} />
             </div>
         </div>
     )
