e9045d9243a50bf9ba261047d97138e829a8e1e6
Limon4egtop <vowa.fvn2005@yandex.ru>
2024-05-27 22:09:31 +0300

    значительные изменения в вводе формы

--- src/components/Users/EditUser/EditUser.tsx
+++ src/components/Users/EditUser/EditUser.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import GoBackIcon from './assets/GoBackIcon.svg'
@@ -7,47 +7,64 @@ import PlaceholderIcon from './assets/PlaceholderIcon.jpg'
 import './EditUser.scss'
 
 const EditUser: React.FC = () => {
-    const navigate = useNavigate()
-    const params = useParams<{uid: string}>()
-    const institute = document.getElementById('institute') as HTMLInputElement | null;
-    const position = document.getElementById('position') as HTMLInputElement | null;
-    const phone = document.getElementById('phone') as HTMLInputElement | null;
-    const role = document.getElementById('role') as HTMLInputElement | null;
-    const email = document.getElementById('email') as HTMLInputElement | null;
-    const last_name = document.getElementById('last-name') as HTMLInputElement | null;
-    const first_name = document.getElementById('first-name') as HTMLInputElement | null;
-    const patronymic = document.getElementById('patronymic') as HTMLInputElement | null;
-    const login = document.getElementById('login') as HTMLInputElement | null;
-    const password_1 = document.getElementById('password-1') as HTMLInputElement | null;
-    const password_2 = document.getElementById('password-2') as HTMLInputElement | null;
-    const password_3 = document.getElementById('password-3') as HTMLInputElement | null;
+    const navigate= useNavigate()
+    const { uid } = useParams();
+    const [formData, setFormData] = useState({
+        institute: '',
+        position: '',
+        phone: '',
+        role: '',
+        email: '',
+        lastName: '',
+        firstName: '',
+        patronymic: '',
+        login: '',
+        password1: '',
+        password2: '',
+        password3: ''
+    });
 
 
+    function handleChange() {
+
+    }
 
     return (
         <div className='user-edit-container'>
             <div className='title-block'>
-                <span className='title'>Editing a user {params.uid}</span>
+                <span className='title'>Editing a user {uid}</span>
             </div>
             <div className='edit-block'>
                 <img onClick={() => navigate('/panel/user/list')} className='back-icon' src={GoBackIcon} />
                 <div className='fields'>
                     <div className='fileds-block'>
                         <img className='profile-icon' src={PlaceholderIcon}/>
-                        <input id='institute' className='input-field'/>
-                        <input id='position' className='input-field'/>
-                        <input id='phone' className='input-field'/>
-                        <input id='role' className='input-field'/>
+                        <input id="institute" className="input-field" value={formData.institute}
+                               onChange={handleChange}/>
+                        <input id="position" className="input-field" value={formData.position}
+                               onChange={handleChange}/>
+                        <input id="phone" className="input-field" value={formData.phone}
+                               onChange={handleChange}/>
+                        <input id="role" className="input-field" value={formData.role}
+                               onChange={handleChange}/>
                     </div>
                     <div className='fileds-block'>
-                        <input id='email' className='input-field'/>
-                        <input id='last-name' className='input-field'/>
-                        <input id='first-name' className='input-field'/>
-                        <input id='patronymic' className='input-field'/>
-                        <input id='login' className='input-field'/>
-                        <input id='password-1' className='input-field'/>
-                        <input id='password-2' className='input-field'/>
-                        <input id='password-3' className='input-field'/>
+                        <input id="email" className="input-field" value={formData.email}
+                               onChange={handleChange}/>
+                        <input id="last-name" className="input-field" value={formData.lastName}
+                               onChange={handleChange}/>
+                        <input id="first-name" className="input-field" value={formData.firstName}
+                               onChange={handleChange}/>
+                        <input id="patronymic" className="input-field" value={formData.patronymic}
+                               onChange={handleChange}/>
+                        <input id="login" className="input-field" value={formData.login}
+                               onChange={handleChange}/>
+                        <input id="password-1" className="input-field" value={formData.password1}
+                               onChange={handleChange}/>
+                        <input id="password-2" className="input-field" value={formData.password2}
+                               onChange={handleChange}/>
+                        <input id="password-3" className="input-field" value={formData.password3}
+                               onChange={handleChange}/>
                     </div>
                 </div>
             </div>
